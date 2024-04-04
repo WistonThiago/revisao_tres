007918a9be4610b391094542d38e6bcc278acecd
WistonThiago <wthyago-2000@hotmail.com>
2024-04-04 20:20:52 -0300

    Resolvida a tarefa 1.

---
+++ array_e_indice.py
@@ -0,0 +1,7 @@
+def mostrarNomes():
+    nomes = ["Maria", "João","Roberto", "Thiago"]
+    print("1 - ", nomes[0])
+    print("2 - ", nomes[1])
+    print("3 - ", nomes[2])
+    print("4 - ", nomes[3])
+mostrarNomes()
