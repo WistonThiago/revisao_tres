7ad0515cc1e6b3f790db870313126adae3439b65
WistonThiago <wthyago-2000@hotmail.com>
2024-04-04 20:26:37 -0300

    Resolvida a tarefa 2.

--- array_e_indice.py
+++ array_e_indice.py
@@ -1,7 +1,14 @@
 def mostrarNomes():
-    nomes = ["Maria", "João","Roberto", "Thiago"]
-    print("1 - ", nomes[0])
-    print("2 - ", nomes[1])
-    print("3 - ", nomes[2])
-    print("4 - ", nomes[3])
+    nomesOne = ["Maria", "João","Roberto", "Thiago"]
+    print("1 - ", nomesOne[0])
+    print("2 - ", nomesOne[1])
+    print("3 - ", nomesOne[2])
+    print("4 - ", nomesOne[3])
 mostrarNomes()
+
+def tarefaDois():
+    nomesTwo = ["Marcos", "Rogério", "Joana", "Rafaela"]
+    print("Primeiro nome: ", nomesTwo[0])
+    print("Último nome: ", nomesTwo[3])
+tarefaDois()
+
