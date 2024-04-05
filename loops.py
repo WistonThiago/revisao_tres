e8581548adb1ae76024d737532f4d1e385efa86c
WistonThiago <wthyago-2000@hotmail.com>
2024-04-04 21:39:29 -0300

    Resolvida a tarefa 6.

--- loops.py
+++ loops.py
@@ -1,4 +1,12 @@
-def contarAteVinte():
+'''def contarAteVinte():
     for i in range(21):
         print(i)
-contarAteVinte()
+contarAteVinte()'''
+
+def contadorUser():
+    number = int(input("Digite um número inteiro: "))
+    counter = 1
+    while (counter <= number):
+        print(counter)
+        counter = counter + 1
+contadorUser()
